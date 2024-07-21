993d742e5e093720325f08e7cd89e80779df31c4
Chyngyz-alimkulov <163808886+Chyngyz-alimkulov@users.noreply.github.com>
2024-07-21 13:21:57 +0600

    Add files via upload

---
+++ defs.py
@@ -0,0 +1,9 @@
+# def name_def(number: int):
+#     if number > 2:
+#         print("+")
+
+# def say_hello():
+#     print("hello!")
+myInt: int = 20
+myStr: str = "Hello"
+myBool: bool = True
---
+++ main.py
@@ -0,0 +1,7 @@
+# from defs import *
+
+
+# print("Тут начинается игра: ")
+
+# say_hello()
+# name_def(2871)
