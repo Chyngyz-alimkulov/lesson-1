00feabc94e891edf848a1929263afbcda3f9bc19
Chyngyz-alimkulov <163808886+Chyngyz-alimkulov@users.noreply.github.com>
2024-08-25 13:39:22 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,74 @@
-from art import text2art
+# x = y = z = 0
 
-text_to_art = input("Enter any text: ")
-print(text2art('SEBAB KEBAB'))
+# print(x, y, z)
+
+
+
+
+
+
+# a, = b, = c, = 10, 20, 30
+
+
+
+
+# a, = b, = c, = [10, 20, 30]
+
+# print(a, b, c)
+
+# a, *b, c = [1, 2]
+
+# print(a)
+# print(b)
+# print(c)
+
+# def hello(first_name, second_name):
+#     print(f"Hello {first_name} { second_name}!")
+
+# hello("John", "Bobrovski")
+
+# def hello(*args):
+#     print(args)
+#     for name in args:
+#         print(f"Hello {name}!")
+
+# hello('alex', ' Stela', 'John', 'Danila')
+
+# def user_info(**kwargs):
+#     print(kwargs)
+
+# print(name='Stiv', age=20, city="Bishkek")
+
+
+# my_dict = {"key": "value", "key1": "value"}
+# phoneBook = {"mom": "090909908", "dad": "18923018"}
+
+# phoneBook["friend"] = "00830284023"
+
+# print(phoneBook["friend"])
+
+# phoneBook['mom'] = "1111111111"
+
+# print(phoneBook)
+# def sum(a):
+#     pass
+
+# phoneBook = {"mom": "09912471", "dad": "92193120"}
+# print(phoneBook.keys())
+# print(phoneBook.values())
+# print(phoneBook.items())
+
+# def user_info(**kwargs):
+#     for key, value in kwargs.items():
+#         print(f"Ключ: {key}, Значение: {value}")
+
+# user_info(name="Alex", age=14)
+
+str1 = "pqdnsqpcn qonqwccqpoxq qconwqondqd qpoqm"
+str2 = ";qmdosnoqinc qcqijnq icsosnq c qsoqs"
+str3 = "cpnsqincqspqncqsps sa csakaso alckaa"
+str4 = "oncoqnosanASPLAA ASMOS  pocmsa"
+str5 = ""
+
+def count_letters(string):
+    g_let = "eoekaoi"
